d8b062154b0fca4a4c5a301dc298f82e3a0f73e3
LIU Zaoqu <68080738+Zaoqu-Liu@users.noreply.github.com>
2025-05-16 08:52:45 +0800

    Add files via upload

---
+++ aividence/__init__.py
@@ -0,0 +1,11 @@
+"""
+AIvidence: Scientific claim validation for LLM-generated content.
+
+Author: Zaoqu Liu
+Email: liuzaoqu@163.com
+GitHub: https://github.com/Zaoqu-Liu
+"""
+
+__version__ = "0.1.0"
+
+from aividence.claim_validator import ClaimValidator
